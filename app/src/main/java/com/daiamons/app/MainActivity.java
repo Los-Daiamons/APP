af8eb82d3dee45ad71d16df9d92470c334ff09c6
Arguelless <marguelles@protonmail.com>
2023-11-16 17:15:49 +0100

    asdf

--- app/src/main/java/com/daiamons/app/MainActivity.java
+++ app/src/main/java/com/daiamons/app/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -12,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     private WebSocketManager webSocketManager;
     private EditText messageEditText;
-    private EditText ipEditText; // Nuevo campo para la dirección IP
+    private EditText ipEditText;
+    private TextView mobileConnectionsTextView;
+    private TextView desktopConnectionsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +26,16 @@ public class MainActivity extends AppCompatActivity {
         Button connectButton = findViewById(R.id.button);
         Button sendButton = findViewById(R.id.button2);
         messageEditText = findViewById(R.id.editText);
-        ipEditText = findViewById(R.id.ipEditText); // Referencia al nuevo EditText
+        ipEditText = findViewById(R.id.ipEditText);
+        mobileConnectionsTextView = findViewById(R.id.mobileConnectionsTextView);
+        desktopConnectionsTextView = findViewById(R.id.desktopConnectionsTextView);
 
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String connectionName = "mobile";
-                String ipAddress = ipEditText.getText().toString(); // Obtener la dirección IP ingresada
-                String serverUrl = "ws://" + ipAddress + ":8887?name=" + connectionName; // Crear la URL con la dirección IP";
+                String ipAddress = ipEditText.getText().toString();
+                String serverUrl = "ws://" + ipAddress + ":8887?name=" + connectionName;
                 connectWebSocket(serverUrl, connectionName);
             }
         });
@@ -44,13 +50,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void connectWebSocket(String serverUrl, String connectionName) {
         try {
-            webSocketManager = new WebSocketManager(new URI(serverUrl));
+            webSocketManager = new WebSocketManager(new URI(serverUrl), mobileConnectionsTextView, desktopConnectionsTextView);
             webSocketManager.setConnectionName(connectionName);
+
             webSocketManager.connect();
+
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
     }
+
+
 
     private void sendMessage() {
         if (webSocketManager != null && webSocketManager.isOpen()) {
--- app/src/main/java/com/daiamons/app/WebSocketManager.java
+++ app/src/main/java/com/daiamons/app/WebSocketManager.java
@@ -1,12 +1,31 @@
 package com.daiamons.app;
+
+import android.app.Activity;
+import android.widget.TextView;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import org.java_websocket.WebSocket;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WebSocketManager extends WebSocketClient {
 
+    private TextView connectionInfoTextView; // Nuevo campo
+    private Map<WebSocket, String> connectionNames = new ConcurrentHashMap<>();
+
     private String connectionName;
+    private TextView mobileConnectionsTextView;
+    private TextView desktopConnectionsTextView;
+    private Gson gson = new Gson();
+    private Activity activity;  // o Context si prefieres
+
+
+
+
 
     public void setConnectionName(String connectionName) {
         this.connectionName = connectionName;
@@ -16,8 +35,12 @@ public class WebSocketManager extends WebSocketClient {
         return connectionName;
     }
 
-    public WebSocketManager(URI serverUri) {
+    public WebSocketManager(URI serverUri, TextView mobileConnectionsTextView, TextView desktopConnectionsTextView) {
         super(serverUri);
+        this.mobileConnectionsTextView = mobileConnectionsTextView;
+        this.desktopConnectionsTextView = desktopConnectionsTextView;
+        this.activity = activity;
+
     }
 
     @Override
@@ -26,6 +49,14 @@ public class WebSocketManager extends WebSocketClient {
 
     @Override
     public void onMessage(String message) {
+        Map<String, Object> data = parseJson(message);
+
+        if (data != null && data.containsKey("type") && data.get("type").equals("connection_count")) {
+            int mobileConnections = Integer.parseInt(data.get("mobile_connections").toString());
+            int desktopConnections = Integer.parseInt(data.get("desktop_connections").toString());
+
+            updateUI(mobileConnections, desktopConnections);
+        }
     }
 
     @Override
@@ -35,4 +66,57 @@ public class WebSocketManager extends WebSocketClient {
     @Override
     public void onError(Exception ex) {
     }
+
+    private void updateAndSendConnectionCount() {
+        int mobileConnections = 0;
+        int desktopConnections = 0;
+
+        for (String name : connectionNames.values()) {
+            if (name.contains("?name=mobile")) {
+                mobileConnections++;
+            } else if (name.contains("?name=desktop")) {
+                desktopConnections++;
+            }
+        }
+        String connectionInfoText = "Conexiones móviles: " + mobileConnections +
+                "\nConexiones de escritorio: " + desktopConnections;
+
+        if (connectionInfoTextView != null) {
+            connectionInfoTextView.setText(connectionInfoText);
+        }
+
+        String message = "{\"type\": \"connection_count\", \"mobile_connections\": " + mobileConnections +
+                ", \"desktop_connections\": " + desktopConnections + "}";
+        broadcast(message);
+    }
+    private void broadcast(String message) {
+        for (WebSocket connection : connectionNames.keySet()) {
+            connection.send(message);
+        }    }
+
+
+    private Map<String, Object> parseJson(String json) {
+        try {
+            return gson.fromJson(json, Map.class);
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void updateUI(int mobileConnections, int desktopConnections) {
+
+        if (activity != null) {
+
+        activity.runOnUiThread(() -> {
+            if (mobileConnectionsTextView != null) {
+                mobileConnectionsTextView.setText("Mobile Connections: " + mobileConnections);
+            }
+            if (desktopConnectionsTextView != null) {
+                desktopConnectionsTextView.setText("Desktop Connections: " + desktopConnections);
+            }
+        });
+    }}
+
+
 }
